75bc3f546f5c23f7e470b2fbdd8331be6d349692
Zhamangulova Adinai <zhamangulovaadinai@Zhamangulovas-MacBook-Air.local>
2024-11-03 17:05:55 +0600

    updated

--- components/ProfileSettingsForm.vue
+++ components/ProfileSettingsForm.vue
@@ -1,314 +1,298 @@
 <template>
-    <section>
-        <img src="../assets/images/gradient-main.svg" alt="gradient">
-        <div class="header">
-        
-            <div class="header-info">
-        
-                <img src="../assets/images/user.svg" alt="user">
-        
-                <div class="header-info-text">
-                    <p>{{form.fullName?.value|| 'Alexa Rawles'  }}
-                    </p>
-                    <p>{{  form.email.value||'alexarawles@gmail.com' }}</p>
-                </div>
-            </div>
-        
-            <div class="header-btn">
-        
-                <button class='blue-btn'  @click="saveForm">Save</button>
-            </div>
-        </div>
-        
+  <section>
+    <img src="../assets/images/gradient-main.svg" alt="gradient" />
+    <div class="header">
+      <div class="header-info">
+        <img src="../assets/images/user.svg" alt="user" />
 
-        
-    
+        <div class="header-info-text">
+          <p>{{ form.fullName?.value || "Alexa Rawles" }}</p>
+          <p>{{ form.email.value || "alexarawles@gmail.com" }}</p>
+        </div>
+      </div>
+
+      <div class="header-btn">
+        <button class="blue-btn transition hover:opacity-80 hover:ease-in duration-300" @click="saveForm">Save</button>
+      </div>
+    </div>
 
     <form class="my-8 grid-container">
-<div v-for="field in formFieldsArray" :key="field?.placeholder" class="my-6">
-    <div  class="form-field flex flex-col gap-3" v-if='field?.type!== "select"' >
-        <label :for="field.key">{{ field.label }}</label>
-        <input
+      <div
+        v-for="field in formFieldsArray"
+        :key="field?.placeholder"
+        class="my-6"
+      >
+        <div
+          class="form-field flex flex-col gap-3"
+          v-if="field?.type !== 'select'"
+        >
+          <label :for="field.key">{{ field.label }}</label>
+          <input
             :id="field.key"
             v-model="form[field.key as keyof typeof form].value"
             :type="field.type"
             :placeholder="field.placeholder"
             :required="field.required"
-            class="form-input focus:border-sky-500"
+             class="form-input transition border-gray-300 focus:border-sky-500 hover:border-sky-500 ease-in duration-300"
             @input="validateField(field.key)"
+          />
+          <span v-if="field.error" class="text-red-500 text-sm">{{
+            field.error
+          }}</span>
+        </div>
+
+        <UISelect
+          :error="form[field.key as keyof typeof form].error"
+          v-else-if="field?.type === 'select'"
+          :values="form[field.key as keyof typeof form].values as any"
+          @selectItem="selectItem"
         />
-        <span v-if="field.error" class='text-red-500 text-sm'>{{ field.error }}</span>
-
-
-    </div>
-
-
-    <UISelect  :error="form[field.key as keyof typeof form].error"  v-else-if='field?.type==="select"' :values="form[field.key as keyof typeof form].values as any" @selectItem="selectItem"/>
-
-</div>
-       
-      
+      </div>
     </form>
-
-        
-</section>
-    
+  </section>
 </template>
 
 <script setup lang="ts">
 interface SelectItem {
-    name: string;
-    value: string;
+  name: string;
+  value: string;
 }
 
 interface SelectProps {
-    label: string;
-    placeholder: string;
-    items: SelectItem[];
-    key:string
-    required?:boolean
+  label: string;
+  placeholder: string;
+  items: SelectItem[];
+  key: string;
+  required?: boolean;
 }
 
 interface FormField {
-    value: string;
-    type: 'text' | 'email' | 'select';
-    required?: boolean;
-    error: string;
-    placeholder?: string;
-    label?: string;
-    values?: SelectProps; 
-
+  value: string;
+  type: "text" | "email" | "select";
+  required?: boolean;
+  error: string;
+  placeholder?: string;
+  label?: string;
+  values?: SelectProps;
 }
 
 type Form = Record<string, FormField>;
 
+const form = ref<Form>({
+  fullName: {
+    value: "",
+    type: "text",
+    required: true,
+    error: "",
+    placeholder: "Your First Name",
+    label: "Full Name",
+  },
 
-const form=ref<Form>({
-    fullName:{
-        value:'',
-        type:'text',
-        required:true,
-        error:'',
-        placeholder:'Your First Name',
-        label:'Full Name',
+  gender: {
+    value: "",
+    type: "select",
+    required: false,
+    error: "",
 
+    values: {
+      key: "gender",
+      label: "Gender",
+      placeholder: "Your Gender",
+      items: [
+        { name: "Female", value: "female" },
+        { name: "Male", value: "male" },
+      ],
     },
+  },
 
-    gender:{
-        value:'',
-        type:'select',
-        required:false,
-        error:'',
-      
-       values:{
-        key:'gender',
-        label:'Gender',
-        placeholder:'Your Gender',
-        items:[
-            {name:'Female',value:'female'},
-            {name:'Male',value:'male'},
+  language: {
+    value: "",
+    type: "select",
+    label: "Language",
+    error: "",
+    required: true,
+    values: {
+      key: "language",
+      label: "Language",
+      placeholder: "Your Language",
 
-        ]
-       }
+      items: [
+        { name: "KG", value: "kg" },
+        { name: "RUS", value: "rus" },
+        { name: "EN", value: "en" },
+      ],
     },
+  },
+  email: {
+    value: "",
+    type: "email",
+    required: true,
+    error: "",
+    label: "Email",
+    placeholder: "Your Email",
+  },
+  country: {
+    value: "",
+    type: "select",
 
-    language:{
-        value:'',
-        type:'select',
-        label:'Language',
-        error:'',
-        required:true,
-       values:{
-        key:'language',
-        label:'Language',
-        placeholder:'Your Language',
-
-        items:[
-            {name:'KG',value:'kg'},
-            {name:'RUS',value:'rus'},
-            {name:'EN',value:'en'},
-
-        ]
-       }
+    error: "",
+    required: false,
+    values: {
+      key: "country",
+      label: "Country",
+      placeholder: "Your Country",
+      items: [
+        { name: "Kyrgyzstan", value: "kg" },
+        { name: "Russia", value: "rus" },
+        { name: "Uzbekistan", value: "uz" },
+      ],
     },
-    email:{
-        value:'',
-        type:'email',
-        required:true,
-        error:'',
-        label:'Email',
-        placeholder:'Your Email',
-   
+  },
+  timeZone: {
+    value: "",
+    type: "select",
+    required: false,
+    error: "",
+
+    values: {
+      key: "timeZone",
+      label: "Time Zone",
+      placeholder: "Your Time Zone",
+      items: [
+        { name: "UTC-6", value: "utc-6" },
+        { name: "UTC-8", value: "utc-8" },
+      ],
     },
-    country:{
-        value:'',
-        type:'select',
-
-        error:'',
-      required:false,
-       values:{
-
-        key:'country',
-        label:'Country',
-        placeholder:'Your Country',
-        items:[
-            {name:'Kyrgyzstan',value:'kg'},
-            {name:'Russia',value:'rus'},
-            {name:'Uzbekistan',value:'uz'},
-        ]
-       }
-    },
-    timeZone:{
-        value:'',
-        type:'select',
-        required:false,
-        error:'',
-      
-       values:{
-        key:'timeZone',
-        label:'Time Zone',
-        placeholder:'Your Time Zone',
-        items:[
-            {name:'UTC-6',value:'utc-6'},
-            {name:'UTC-8',value:'utc-8'},
-     
-        ]
-       }
-    },
-})
-
-const validateField = (fieldKey:string) => {
-    const field = form.value[fieldKey];
-    field.error = ''; 
-
-    if (field.required && !field.value) {
-        field.error = `${field.label} is required.`;
-    } else {
-        switch (field.type) {
-            case 'text':
-                if (!field.value.trim()) {
-                    field.error = `${field.label} cannot be empty.`;
-                }
-                break;
-            case 'email':
-                const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                if (!emailPattern.test(field.value)) {
-                    field.error = `${field.label} must be a valid email address.`;
-                }
-                break;
-            default:
-                break;
-        }
-    }
-};
-
-
-
-const formFieldsArray = computed(() => {
-    return Object.entries(form.value).map(([key, field]) => ({
-        key,
-        ...field
-    }));
+  },
 });
 
+const validateField = (fieldKey: string) => {
+  const field = form.value[fieldKey];
+  field.error = "";
 
+  if (field.required && !field.value) {
+    field.error = `${field.label} is required.`;
+  } else {
+    switch (field.type) {
+      case "text":
+        if (!field.value.trim()) {
+          field.error = `${field.label} cannot be empty.`;
+        }
+        break;
+      case "email":
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(field.value)) {
+          field.error = `${field.label} must be a valid email address.`;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+};
 
-const saveForm=()=>{
+const formFieldsArray = computed(() => {
+  return Object.entries(form.value).map(([key, field]) => ({
+    key,
+    ...field,
+  }));
+});
 
-for (const key in form.value) {
+const saveForm = () => {
+  for (const key in form.value) {
     validateField(key);
-}
+  }
 
-const isValid = Object.values(form.value).every(field => !field.error);
-if(isValid){
-    alert('Success')
-}else{
+  const isValid = Object.values(form.value).every((field) => !field.error);
+  if (isValid) {
+    alert("Success");
+  } else {
     return;
-}
+  }
+};
 
-}
+const selectItem = (item: SelectItem, key: keyof typeof form.value) => {
+  console.log(item, key);
+  form.value[key].value = item.value;
 
-const selectItem =(item:SelectItem, key:keyof typeof form.value)=>{
-console.log(item,key)
-form.value[key].value = item.value
-
-form.value[key].error=''
-
-}
-
+  form.value[key].error = "";
+};
 </script>
 
-<style scoped lang='scss'>
-.form-input{
-    background: #F9F9F9;
-    border-radius: 8px;
-    @extend %basic-md-text;
-    padding:14px 20px;
+<style scoped lang="scss">
+.form-input {
+  background: #f9f9f9;
+  border-radius: 8px;
+  @extend %basic-md-text;
+  padding: 14px 20px;
+
+  &::placeholder{
+    color:#00000040
+  }
 }
 .grid-container {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr); 
-    grid-template-rows: repeat(3, auto); 
-    gap: 16px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, auto);
+  gap: 16px;
 }
 
 .form-field {
+  display: flex;
+  flex-direction: column;
+}
+
+section {
+  width: 100%;
+  padding: 40px;
+}
+
+.blue-btn {
+  background: #4182f9;
+  color: white;
+  padding: 10px 21px;
+  border-radius: 8px;
+  font-size: 16px;
+  line-height: 24px;
+
+ 
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 32px;
+  &-info {
     display: flex;
-    flex-direction: column;
-}
-
-
-section{
-    width: 100%;
-    padding:40px;
-}
-
-.blue-btn{
-    background: #4182F9;
-color:white;
-padding:10px 21px 10px 32px;
-border-radius: 8px;
-font-size: 16px;
-line-height: 24px;
-}
-
-
-.header{
-    display: flex;
-    justify-content: space-between;
+    gap: 32px;
     align-items: center;
-    margin-top: 32px;
-    &-info{
-        display: flex;
-        gap:32px;
-        align-items:center;
-    }
+  }
 }
 
-.header-info-text p:first-child{
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 30px;
+.header-info-text p:first-child {
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 30px;
 }
 
-.header-info-text p:last-child{
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 24px;
-    color:#00000040;
+.header-info-text p:last-child {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #00000040;
 }
 
-@media (max-width:600px){
-    .header{
-        flex-direction: column;
-        align-items: start;
-        gap:20px
-    }
+@media (max-width: 600px) {
+  .header {
+    flex-direction: column;
+    align-items: start;
+    gap: 20px;
+  }
 
-    .grid-container{
-        grid-template-columns: 1fr; 
-        grid-template-rows: repeat(3, auto); 
-    }
+  .grid-container {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, auto);
+  }
 }
-    
 </style>
--- components/UI/Select.vue
+++ components/UI/Select.vue
@@ -1,105 +1,111 @@
 <template>
-    <div class="select">
-      <div class="flex flex-col gap-3">
-        <span class="basic-md-text"  >{{ values?.label }}</span>
-    <div class="selected-block" @click="()=>toggleOpen()">
- <p class='selected-text'>{{ selectedItem }}</p>
+  <div class="select">
+    <div class="flex flex-col gap-3">
+      <span class="basic-md-text">{{ values?.label }}</span>
+      <div class="selected-block hover:cursor-pointer" @click="toggleOpen" >
+        <p class="selected-text hover:cursor-pointer"  :class="{'after-select':isItemSelected}">{{ selectedItem }}</p>
 
+        <img src="../../assets/images/arrow-down.svg" alt="arrow-down" />
 
-        <img src="../../assets/images/arrow-down.svg" alt="arrow-down">
-     </div>
-
-
-     <span v-if="error" class='text-red-500 text-sm'>{{error }}</span>
       </div>
 
-      <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
-      <div v-if="isOpen" class="select-options flex flex-col gap-2 my-2">
-      <p  v-for="item in values.items" :key="item?.value" @click="selectItem(item)" class="p-1 group/item hover:bg-slate-100">{{ item?.name }}</p>
-      </div>
-</transition>
+      <span v-if="error" class="text-red-500 text-sm">{{ error }}</span>
     </div>
+
+    <transition
+      leave-active-class="transition ease-in duration-100"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+    >
+      <div v-if="isOpen" class="select-options flex flex-col gap-2 my-2">
+        <div
+          v-for="item in values.items"
+          :key="item?.value"
+          @click="selectItem(item)"
+          class="p-1 group/item hover:bg-slate-100 hover:cursor-pointer"
+        >
+       {{ item?.name }}
+
+      </div>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script setup lang="ts">
-const isOpen=ref(false)
+const isOpen = ref(false);
 
-
-export type SelectItem={
-    name:string,
-    value:string
-}
-const props=defineProps({
-    values:{
-        type: Object as PropType<SelectProps | never>,
-        required:true
-        
-    },
-    error:String
-})
+export type SelectItem = {
+  name: string;
+  value: string;
+};
+const props = defineProps({
+  values: {
+    type: Object as PropType<SelectProps | never>,
+    required: true,
+  },
+  error: String,
+});
 
 const emit = defineEmits<{
-
-  selectItem: [SelectItem,string]
-  
-}>()
-export type SelectProps={
-    label:string,
-    placeholder:string,
-    items:SelectItem[];
-    key:string
-    required?:boolean
-}
+  selectItem: [SelectItem, string];
+}>();
+export type SelectProps = {
+  label: string;
+  placeholder: string;
+  items: SelectItem[];
+  key: string;
+  required?: boolean;
+};
+const isItemSelected=ref(false)
 
 const selectedItem = ref(props?.values?.placeholder);
 
-const selectItem = (item:SelectItem)=>{
-    selectedItem.value=item?.name
+const selectItem = (item: SelectItem) => {
+  selectedItem.value = item?.name;
 
-emit('selectItem',item, props.values.key)
-    isOpen.value=false
-}
+  emit("selectItem", item, props.values.key);
+  isOpen.value = false;
+  isItemSelected.value=true
+};
 
-
-
-const toggleOpen =()=>{
-    isOpen.value=!isOpen.value
-
-
-
-}
-
-
+const toggleOpen = () => {
+  isOpen.value = !isOpen.value;
+};
 </script>
 
 <style scoped lang="scss">
+.selected-text {
+  background: #f9f9f9;
+  border-radius: 8px;
+  @extend %basic-md-text;
+  padding: 14px 20px;
+  color:#00000040;
 
-.selected-text{
-    background: #F9F9F9;
-    border-radius: 8px;
-    @extend %basic-md-text;
-    padding:14px 20px;
-    
+
+
 }
-.select-options{
-    background: #F9F9F9;
-    padding:10px 15px;
-border-radius: 8px;
-@extend %basic-md-text;
+.after-select{
+  color:black
+}
+.select-options {
+  background: #f9f9f9;
+  padding: 10px 15px;
+  border-radius: 8px;
+  @extend %basic-md-text;
 }
 
+.selected-block {
+  position: relative;
 
-
-.selected-block{
-    position: relative;
-
-.selected-text{
+  .selected-text {
     width: 100%;
-}
-img{
+  }
+  img {
     position: absolute;
-    top:22px;
-    right:24px;
-}
+    top: 22px;
+    right: 24px;
+
+  }
 }
 </style>
